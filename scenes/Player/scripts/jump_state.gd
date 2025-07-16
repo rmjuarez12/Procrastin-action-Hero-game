43a1e3ae3ffe61f0b1cb20da8b89399c075562ef
Richard Rodriguez <rmjuarez12@gmail.com>
2025-07-16 11:19:15 -0400

    Fixed the jump state not showing up

--- scenes/Player/scripts/jump_state.gd
+++ scenes/Player/scripts/jump_state.gd
@@ -5,20 +5,16 @@ extends PlayerState
 @export var falling_state: PlayerState
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	# print(character.velocity.y)
-
-	if character.velocity.y > 100:
+func state_process(_delta: float) -> void:
+	if character.velocity.y > 0:
 		playback.travel("falling")
 		next_state = falling_state
 		next_state.can_move = true
 
-func on_exit():
-	print("Exiting Jump State")
-
-# func state_input(event : InputEvent):
-# 	if(event.is_action_released("jump")):
-# 		_cancel_jump()
+func state_input(event : InputEvent):
+	if(event.is_action_released("jump")):
+		_cancel_jump()
 
 func _cancel_jump():
+	print("Jump cancelled")
 	character.velocity.y *= decelerate_on_jump_release
